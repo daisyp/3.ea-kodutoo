4d4bdd9c323801fa54b598cfc20abb1880b741f5
kurmo <kurmo14@tlu.ee>
2018-05-25 11:40:30 +0300

    fixed

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,33 +7,34 @@ function result() {
     from = $('#convertFrom').find(":selected").text();
     to = $('#convertTo').find(":selected").text();
 
-    $.getJSON("https://api.coinmarketcap.com/v1/ticker/" + from, getFrom);
-    $.getJSON("https://api.coinmarketcap.com/v1/ticker/" + to, getTo);
-    
-    var discount = +((3/2).toFixed(2));
-    $('#result').text(discount);
+    $.getJSON("https://api.coinmarketcap.com/v1/ticker/" + from, function (json) {
+      for (elem in json) {
+        j = json[elem]['price_usd'];
+      }
+      console.log(j);
+    });
+    $.getJSON("https://api.coinmarketcap.com/v1/ticker/" + to, function (json) {
+      for (elem in json) {
+        k = json[elem]['price_usd'];
+      }
+      console.log(k);
+      finalResult();
 
-    let fadeTime = 200;
-    console.log("s" + j + "d" + k);
-
-    $('.all').css({"filter": "blur(5px)"})
-    $('.popupResult').fadeIn(fadeTime)
-    
+    });
 }
 
-function getFrom(json) {
-    for (elem in json) {
-        var j = json[elem]['price_usd']
-    }
-    console.log(j);
-    return parseInt(j);
-}
-function getTo(json) {
-    for (elem in json) {
-        var k = json[elem]['price_usd']
-    }
-    console.log(k);
-    return parseInt(k);
+function finalResult () {
+  console.log(j);
+  console.log(k);
+  var discount = +((j/k).toFixed(2));
+  console.log(discount);
+  $('#result').text(discount);
+
+  let fadeTime = 200;
+  console.log("s" + j + "d" + k);
+
+  $('.all').css({"filter": "blur(5px)"});
+  $('.popupResult').fadeIn(fadeTime);
 }
 
 function getData(i) {
@@ -51,7 +52,7 @@ function fillUpOptions(json) {
 
     for(elem in json) {
 
-        let newOption = "<option value='" + json[elem]['id'] + "'>" + json[elem]['id'] + "</option>"
+        let newOption = "<option value='" + json[elem]['id'] + "'>" + json[elem]['id'] + "</option>";
 
         $("#convertFrom").append(newOption);
         $("#convertTo").append(newOption);
